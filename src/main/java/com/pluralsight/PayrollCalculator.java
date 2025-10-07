9eedf1a78dae59353bf53e7dc3771f3e134d9d7a
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-07 11:23:00 -0400

    initial comment

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,7 +1,13 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+
 public class PayrollCalculator {
     public static void main(String[] args) {
+        try (BufferedReader bufReader = new BufferedReader(filereader)) {
+        }
+
+
 
     }
 }
